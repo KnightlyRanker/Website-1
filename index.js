5728d747b807712945f1086d271d6ca4e59a9f4d
DomiGrod <Dominick.Grodkowski@gmx.de>
2024-06-18 10:51:48 +0200

    chore: Add Background animation

--- index.js
+++ index.js
@@ -1,39 +1,133 @@
-function getRemainingTime(et) {
-    var dt = Date.parse(et) - Date.parse(new Date());
-    var seconds = Math.floor((dt / 1000) % 60);
-    var minutes = Math.floor((dt / 1000 / 60) % 60);
-    var hours = Math.floor((dt / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(dt / (1000 * 60 * 60 * 24));
-    return { days, hours, minutes, seconds };
+let start = new Date().getTime();
+
+const originPosition = { x: 0, y: 0 };
+
+const last = {
+    starTimestamp: start,
+    starPosition: originPosition,
+    mousePosition: originPosition
 }
 
-function initRemainingTime(id, endTime) {
-    var clock = document.getElementById(id);
-    var daysSpan = clock.querySelector('.days');
-    var hoursSpan = clock.querySelector('.hours');
-    var minutesSpan = clock.querySelector('.minutes');
-    var secondsSpan = clock.querySelector('.seconds');
+const config = {
+    starAnimationDuration: 1500,
+    minimumTimeBetweenStars: 250,
+    minimumDistanceBetweenStars: 75,
+    glowDuration: 75,
+    maximumGlowPointSpacing: 10,
+    colors: ["249 146 253", "252 254 255"],
+    sizes: ["1.4rem", "1rem", "0.6rem"],
+    animations: ["fall-1", "fall-2", "fall-3"]
+}
 
-    function updateRemainingTime() {
-        var t = getRemainingTime(endTime);
-        daysSpan.innerHTML = ('0' + t.days).slice(-2);
-        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+let count = 0;
+  
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min,
+      selectRandom = items => items[rand(0, items.length - 1)];
 
-        if (t.total <= 0) {
-            clearInterval(timeInterval);
-            daysSpan.innerHTML = '000';
-            hoursSpan.innerHTML = '00';
-            minutesSpan.uinnerHTML = '00';
-            secondsSpan.innerHTML = '00';
-        }
+const withUnit = (value, unit) => `${value}${unit}`,
+      px = value => withUnit(value, "px"),
+      ms = value => withUnit(value, "ms");
+
+const calcDistance = (a, b) => {
+    const diffX = b.x - a.x,
+          diffY = b.y - a.y;
+  
+    return Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
+}
+
+const calcElapsedTime = (start, end) => end - start;
+
+const appendElement = element => document.body.appendChild(element),
+      removeElement = (element, delay) => setTimeout(() => document.body.removeChild(element), delay);
+
+const createStar = position => {
+    const star = document.createElement("span"),
+          color = selectRandom(config.colors);
+  
+    star.className = "star fa-solid fa-sparkle";
+  
+    star.style.left = px(position.x);
+    star.style.top = px(position.y);
+    star.style.fontSize = selectRandom(config.sizes);
+    star.style.color = `rgb(${color})`;
+    star.style.textShadow = `0px 0px 1.5rem rgb(${color} / 0.5)`;
+    star.style.animationName = config.animations[count++ % 3];
+    star.style.starAnimationDuration = ms(config.starAnimationDuration);
+  
+    appendElement(star);
+
+    removeElement(star, config.starAnimationDuration);
+}
+
+const createGlowPoint = position => {
+    const glow = document.createElement("div");
+  
+    glow.className = "glow-point";
+  
+    glow.style.left = px(position.x);
+    glow.style.top = px(position.y);
+  
+    appendElement(glow)
+  
+    removeElement(glow, config.glowDuration);
+}
+
+const determinePointQuantity = distance => Math.max(
+    Math.floor(distance / config.maximumGlowPointSpacing),
+    1
+);
+
+const createGlow = (last, current) => {
+    const distance = calcDistance(last, current),
+          quantity = determinePointQuantity(distance);
+  
+    const dx = (current.x - last.x) / quantity,
+          dy = (current.y - last.y) / quantity;
+  
+    Array.from(Array(quantity)).forEach((_, index) => { 
+        const x = last.x + dx * index, 
+              y = last.y + dy * index;
+    
+        createGlowPoint({ x, y });
+    });
+}
+
+const updateLastStar = position => {
+    last.starTimestamp = new Date().getTime();
+
+    last.starPosition = position;
+}
+
+const updateLastMousePosition = position => last.mousePosition = position;
+
+const adjustLastMousePosition = position => {
+    if(last.mousePosition.x === 0 && last.mousePosition.y === 0) {
+        last.mousePosition = position;
     }
+};
 
-    updateRemainingTime();
-    var timeInterval = setInterval(updateRemainingTime, 1000);
+const handleOnMove = e => {
+    const mousePosition = { x: e.clientX, y: e.clientY }
+  
+    adjustLastMousePosition(mousePosition);
+  
+    const now = new Date().getTime(),
+          hasMovedFarEnough = calcDistance(last.starPosition, mousePosition) >= config.minimumDistanceBetweenStars,
+          hasBeenLongEnough = calcElapsedTime(last.starTimestamp, now) > config.minimumTimeBetweenStars;
+  
+    if(hasMovedFarEnough || hasBeenLongEnough) {
+        createStar(mousePosition);
+    
+        updateLastStar(mousePosition);
+    }
+  
+    createGlow(last.mousePosition, mousePosition);
+  
+    updateLastMousePosition(mousePosition);
 }
 
-// Set the countdown to any days from now
-var timeForBigDay = new Date(new Date().getTime() + 99 * 24 * 60 * 60 * 1000);
-initRemainingTime('reminder-clock', timeForBigDay);
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
+
+document.body.onmouseleave = () => updateLastMousePosition(originPosition);
